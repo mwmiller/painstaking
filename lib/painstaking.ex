a54149a194a8a69f0b71b0f030f5c058098a1c51
Matt Miller <mwm@fastmail.com>
2016-01-09 11:20:07 +0200

    Add arb size

--- lib/painstaking.ex
+++ lib/painstaking.ex
@@ -7,10 +7,22 @@ defmodule PainStaking do
     Float.round(bankroll * kelly_fraction(prob, win), 2)
   end
 
+  def arb_size(max_outlay, mutually_exclusives) do
+    if arb_exists(mutually_exclusives) do
+      sizes = mutually_exclusives |> Enum.map(fn(x) -> size_to_collect(x, max_outlay) end)
+      {max_outlay - Enum.sum(sizes) |> Float.round(2), sizes}
+    else
+      {0.0, List.duplicate(0.00, Enum.count(mutually_exclusives))}
+    end
+  end
+
   defp extract_value(kwl, into) do
     [type|_] = Keyword.keys(kwl)
     Exoddic.convert(kwl[type], from: type, to: into, for_display: false)
   end
+
+  defp size_to_collect(offer, goal), do: (goal / (offer |> extract_value(:eu))) |> Float.round(2)
+  defp arb_exists(mutually_exclusives), do: Enum.count(mutually_exclusives) > 1 and mutually_exclusives |> Enum.map(fn(x) -> extract_value(x,:prob) end) |> Enum.sum < 1
 
   defp kelly_fraction(prob,payoff) do
     # Presume we cannot get the other side at the same odds
--- test/painstaking_test.exs
+++ test/painstaking_test.exs
@@ -9,4 +9,11 @@ defmodule PainStakingTest do
     assert PainStaking.kelly_size(15000, [prob: "0.50"], [us: -110])   ==    0.00, "Cannot beat the vig with coin-flips"
   end
 
+  test "simple arb_size" do
+    assert PainStaking.arb_size(1000, [[us: "-110"]])                               ==  {0.0, [0.0]}, "No arbitrage exists on a single outcome"
+    assert PainStaking.arb_size(1000, [[us: "-110"], [us: "-110"]])                 ==  {0.0, [0.0, 0.0]}, "Standard US odds exhibit no arbitrage"
+    assert PainStaking.arb_size(1000, [[us: "-107"], [us: "+110"]])                 ==  {6.9, [516.91, 476.19]}, "Can make a small profit on small arbitrage"
+    assert PainStaking.arb_size(1000, [[us: "-161"], [us: "+350"], [us: "+632"]])   ==  {24.31, [616.86, 222.22, 136.61]}, "Multi-ways are harder to spot so may exhibit more profit"
+  end
+
 end
