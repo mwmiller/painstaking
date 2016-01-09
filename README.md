f66d4d2b499ab015ad85e6ab4c6159ca6585dc42
Matt Miller <mwm@fastmail.com>
2016-01-09 11:39:40 +0200

    Add documentation

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # PainStaking
 
-As yet unreleased code to do bet stake sizing in Elixir:
+Bet stake sizing in Elixir:
 
 - Kelly Criterion
 - Arbitrage
 
+## Examples
+
+```
+PainStaking.kelly_size(20000, [prob: 0.54], [us: "-120"])
+{false, 0.0} # no edge, no bet
+PainStaking.kelly_size(20000, [prob: 0.55], [us: "-120"])
+{true, 200.0} # small edge, small bet
+
+PainStaking.arb_size(1000, [[us: "+100"], [eu: 2.00]])
+false, [0.0, 0.0], 0.0} # No arb, 0 on each, net 0
+PainStaking.arb_size(1000, [[us: "+100"], [eu: 2.10]])
+{true, [500.0, 476.19], 23.81} # Arb available, bet on each, net 23.81 regardless of outcome
+```
--- lib/painstaking.ex
+++ lib/painstaking.ex
@@ -1,13 +1,49 @@
 defmodule PainStaking do
   require Exoddic
+  @moduledoc """
+  Calculate stakes in advantage betting situations
+  """
 
+  @doc """
+  Determine the amount to stake on a single advantage situation based on the
+  estimated edge and the Kelly Criterion
+
+  `bankroll` is the total amount available for wagering
+  `estimate` is the estimated actual odds on the event occurring
+  `offered`  is the odds offered by the counter-party
+
+  `estimate` and `odds` should be entered as atoms and numbers which can be
+  interpreted by the `Exoddic` module. Examples include:
+
+  `[prob: 0.50]` as a probability
+  `[us: "+120"]` as moneyline-style odds
+  `[eu: 2.25]`   as decimal-style odds
+
+  Returns {whether an edge exists, amount to wager}
+  """
+  @spec kelly_size(number, [atom: number], [atom: number]) :: {boolean, float}
   def kelly_size(bankroll, estimate, offered) do
     prob = extract_value(estimate, :prob)
     win = extract_value(offered, :uk)
     amount = Float.round(bankroll * kelly_fraction(prob, win), 2)
     {amount != 0.0, amount}
   end
+  @doc """
+  Determine how much to bet on each of a set of mutually exclusive outcomes in
+  an arbitrage situation.
 
+  `max_outlay` is the maximum available to stake on this set of outcomes.
+  The smaller the arbitrage, the closer your outlay will be to this number.
+
+  `mutually_exclusives` is a list of mutually exclusive outcomes and the odds
+  offered on each.  Each element of the list should be an atom-number keyword list
+  which can be interpreted by `Exoddic`. For example, `[us: "-120"]`
+
+  Returns {whether an arb exists, [stake on each outcome], expected profit}
+
+  The expected profit may suffer from small rounding errors.
+  """
+  @spec arb_size(number, [[atom: number]]) :: {boolean, [float], float}
   def arb_size(max_outlay, mutually_exclusives) do
     if arb_exists(mutually_exclusives) do
       sizes = mutually_exclusives |> Enum.map(fn(x) -> size_to_collect(x, max_outlay) end)
