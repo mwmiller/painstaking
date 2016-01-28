b57de14747d8f7d920af37b96a5b0f755d3c39ab
Matt Miller <mwm@fastmail.com>
2016-01-28 13:27:58 +0200

    Spec everything out.
    Also arb_exists becomes arb_exists?

--- lib/painstaking.ex
+++ lib/painstaking.ex
@@ -45,6 +45,7 @@ defmodule PainStaking do
   """
   @type staking_options :: [bankroll: number, independent: boolean]
 
+  @spec extract_staking_options(staking_options) :: {number, boolean}
   defp extract_staking_options(opts) do
       {Keyword.get(opts, :bankroll, 100), Keyword.get(opts, :independent, false)}
   end
@@ -72,22 +73,26 @@ defmodule PainStaking do
     end
   end
 
+  @spec pick_optimal_set([tuple],[tuple]) :: [tuple]
   defp pick_optimal_set([], acc), do: Enum.reverse acc
   defp pick_optimal_set([this|rest], acc) do
     if single_ev(this,1) > rr(acc), do: pick_optimal_set(rest, [this|acc]), else: pick_optimal_set([], acc)
   end
 
+  @spec resize_fracs([tuple]) :: [tuple]
   defp resize_fracs(fracs) do
     winners = Enum.filter(fracs, fn({_,x}) -> x > 0 end)
     total = winners |> Enum.reduce(0, fn({_,x}, acc) -> x+acc end)
     if (total > 1), do: winners |> Enum.map(fn({d,x}) -> {d, x/total} end), else: winners
   end
 
+  @spec fracs_display([tuple], number, list) :: [tagged_number]
   defp fracs_display([], _,acc), do: Enum.reverse acc
   defp fracs_display([{d,f}|t],b, acc), do: fracs_display(t,b,[{d, Float.round(f*b,2)}|acc])
 
   # The "reserve rate" above which any additions to the set must be
   # in order to be included in the optimal set
+  @spec rr([edge]) :: float
   defp rr([]), do: 1.0 # First must merely be positive expectation
   defp rr(included) do
     {prob_factor, pay_factor} = included |> Enum.reduce({1,1}, fn({_,p,o}, {x,y}) ->
@@ -96,11 +101,13 @@ defmodule PainStaking do
     prob_factor /  pay_factor
   end
 
+  @spec extract_price_value(wager_price, atom) :: number
   defp extract_price_value(kwl, into) do
     [type|_] = Keyword.keys(kwl)
     Exoddic.convert(kwl[type], from: type, to: into, for_display: false)
   end
 
+  @spec kelly_fraction(edge, float | nil) :: float
   defp kelly_fraction({_,p,o}, rr) do
     odds = extract_price_value(o, :eu)
     if odds == 0 do
@@ -121,7 +128,7 @@ defmodule PainStaking do
   @spec arb([edge], staking_options) :: {:ok, [tagged_number], float} | {:error, String.t}
   def arb(mutually_exclusives, opts \\ []) do
     {max_outlay, independent} = extract_staking_options(opts)
-    if arb_exists(mutually_exclusives) and not independent do
+    if arb_exists?(mutually_exclusives) and not independent do
       sizes = mutually_exclusives |> Enum.map(fn({d,_,o}) -> {d, size_to_collect(o, max_outlay)} end)
       {:ok, sizes, sizes |> Enum.reduce(max_outlay, fn({_,x},acc) -> acc - x end) |> Float.round(2)}
     else
@@ -129,12 +136,15 @@ defmodule PainStaking do
     end
   end
 
+  @spec size_to_collect(wager_price, number) :: float
   defp size_to_collect(offer, goal), do: (goal / (offer |> extract_price_value(:eu))) |> Float.round(2)
-  defp arb_exists(mutually_exclusives), do: Enum.count(mutually_exclusives) > 1 and mutually_exclusives |> Enum.reduce(0,fn({_,_,o}, acc) -> extract_price_value(o,:prob)+acc end) < 1
+  @spec arb_exists?([edge]) :: boolean
+  defp arb_exists?(mutually_exclusives), do: Enum.count(mutually_exclusives) > 1 and mutually_exclusives |> Enum.reduce(0,fn({_,_,o}, acc) -> extract_price_value(o,:prob)+acc end) < 1
 
 
   # This seems way more complex than it ought to be.
-  @spec edge_cdf([edge], boolean) :: [{[float], float}]
+  @typep cdf :: [{[float], float}]
+  @spec edge_cdf([edge], boolean) :: cdf
   defp edge_cdf(edges, independent) do
     payoffs = edges |> Enum.map(fn({_,p,o}) -> {extract_price_value(o, :eu), extract_price_value(p, :prob)} end)
     possibles = if independent do
@@ -147,17 +157,20 @@ defmodule PainStaking do
     possibles |> map_prob([], 0)
   end
 
-  defp zero_except(_,[], acc), do: acc
+  @spec zero_except(non_neg_integer, [tuple], tuple) :: tuple
+  defp zero_except(_,[],acc), do: acc
   defp zero_except(n,[{v,p}|t],{vals,j}) do
     {newval, newprob} = if Enum.count(vals) == n, do: {v,p}, else: {0,0}
     zero_except(n,t,{Enum.into([newval], vals), j + newprob})
   end
 
+  @spec pick_combo(non_neg_integer, [tuple], tuple) :: tuple
   defp pick_combo(_, [], acc), do: acc
   defp pick_combo(n,[{v,p}|t],{vals,j}) do
     {newval, newprob} = if ((n >>> Enum.count(vals) &&& 1)) != 0, do: {v,p}, else: {0,1-p}
     pick_combo(n,t,{Enum.into([newval], vals), j * newprob})
   end
+  @spec map_prob([tuple], list, number) :: [tuple]
   defp map_prob([], acc, _), do: acc
   defp map_prob([{l,p}|t], acc, j) do
     limit = j+p
@@ -179,6 +192,7 @@ defmodule PainStaking do
     {:ok, ev - (wagers |> Enum.map(fn({_,a}) -> a end) |> Enum.sum) |> Float.round(2)}
   end
 
+  @spec sample_ev(cdf, [tagged_number], non_neg_integer) :: float
   defp sample_ev(cdf, fracs, iters) do
       total = gather_results(cdf, iters, []) |>  Enum.reduce(0, fn(x, a) -> add_result_row(x,fracs,a) end)
       total / iters
@@ -194,16 +208,21 @@ defmodule PainStaking do
     {mult, _ } = extract_staking_options(opts)
     {:ok, ev_loop(edges,mult,[])}
   end
+  @spec ev_loop([edge], float, list) :: [tagged_number]
   defp ev_loop([],_, acc), do: Enum.reverse acc
   defp ev_loop([{d,p,o}|t],m, acc), do: ev_loop(t, m, [{d, single_ev({d,p,o},m)}|acc])
+  @spec single_ev(edge, number) :: float
   defp single_ev({_,p,o},m), do: m * extract_price_value(p, :prob) * extract_price_value(o, :eu)
 
+  @spec gather_results(cdf, non_neg_integer, list) ::  list
   defp gather_results(_, 0, acc), do: Enum.reverse acc
   defp gather_results(cdf, n, acc), do: gather_results(cdf, n-1, [sample_result(cdf)|acc])
 
+  @spec add_result_row(cdf, [tagged_number], float) :: float
   defp add_result_row(_,[],acc), do: acc
-  defp add_result_row([h|t],[{_,f}|r], acc), do: add_result_row(t,r, h*f+acc)
+  defp add_result_row([h|t],[{_,f}|r],acc), do: add_result_row(t,r,h*f+acc)
 
+  @spec sample_result(cdf) :: [number]
   defp sample_result(cdf) do
     pick = :random.uniform
     case cdf |> Enum.split_while(fn({_,plim}) -> pick > plim end) do
@@ -212,7 +231,9 @@ defmodule PainStaking do
     end
   end
 
+  @spec proper_loss(cdf) :: [number]
   defp proper_loss([{list,_}|_]), do: zeroed(list,[])
+  @spec zeroed(list, [0]) :: [0]
   defp zeroed([], acc), do: acc
   defp zeroed([_|t], acc), do: zeroed(t, [0|acc])
 
